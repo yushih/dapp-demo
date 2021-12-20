ec91919deab3bc485a364eade35416811ac65b9d
yushi <yushihuang@gmail.com>
2021-12-20 11:34:25 +0800

    connect to Yoroi and get balance

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [api, setApi ] = useState(null);
+  const [balance, setBalance] = useState(0);
+
+  const getBalance = async () => {
+    const balance = await api.get_balance().then(balance => {
+      setBalance(balance)
+    })
+  }
+
+  useEffect(() => {
+    window.cardano.yoroi.enable({ requestIdentification: false, onlySilent: false }).then(
+      api => {
+        console.log('successful silent reconnection')
+        setApi(api)
+      },
+      err => {
+        if (String(err).includes('onlySilent:fail')) {
+          console.log('no silent re-connection available');
+        } else {
+          console.error('Silent reconnection failed for unknown reason!', err);
+        }
+      }
+    );
+  })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Balance: {balance} </div>
+      <button onClick={getBalance} disabled={!api}>
+        Get balance
+      </button>
+      <div>2</div>
     </div>
   );
 }
 
 export default App;
+
+
